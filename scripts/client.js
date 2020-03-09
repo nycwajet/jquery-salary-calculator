038c6185ca1017aa7484b2742556ba266640db67
Henry Marin <henrymarin@rocketmail.com>
2020-03-08 20:10:08 -0500

    second DOM try

--- scripts/client.js
+++ scripts/client.js
@@ -2,44 +2,35 @@ console.log('JS');
 
 $( document ).ready( readyNow )
 
-let employeeData = [{firstName, lastName, idNumber, employeeTitle, employeeSalary}];
+
+
+
+appendEmployeeToDom();
+$('#btn-submit').on('click', addEmployee);
+$('#employeeTable').on('click', '#btn-delete', deleteEmployee);
+
+let employeeData = [];
+
 
     function readyNow(){
         console.log('JQ');
 
 
-$('.inputForm').click(function(addEmployee){
-    let $item = $(this).closest('tr')
-                       .find('.nr')
-                       .text();
-        $(this).append($item);
-})
-
-$('#btn-submit').on('click', addEmployee);
-$('#employeeTable').on('click', '#btn-delete', deleteEmployee);
-
+   function addEmployee(firstName, lastName, idNumber, employeeTitle, employeeSalary){
+    console.log('in addEmployee');
+   let employeeData = {firstName, lastName, idNumber, employeeTitle, employeeSalary};
+   console.log('New employee is', employee);
+   employeeData.push(employee);
+}
 
 
 function appendEmployeeToDom(){
+    console.log('in append to DOM');
 let tableEntry = $('#employeeTable');
-
 tableEntry.empty();
-
-for (item of employeeData){
-    let $td=$('
-    <tr>
-    <td>${item.firstName}</td>
-    <td>${item.lastName}</td>
-    <td>${item.idNumber}</td>
-    <td>${item.employeeTitle}</td>
-    <td>${item.employeeSalary}</td>
-    </tr>'
-    );
-
-    tableEntry.append($td);
+}
 }
    
-}
 
 
 
